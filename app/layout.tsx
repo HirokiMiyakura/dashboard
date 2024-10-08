5cf7334535cf7a7b184460e698a8e58ae57459fa
HirokiMiyakura <miyakurahiroki@gmail.com>
2024-10-08 13:11:43 +0900

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import '@/app/ui/global.css';
+import { inter } from './ui/fonts';
+
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	// :以降は型定義
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${inter.className} antialiased`}>{children}</body>
+		</html>
+	);
 }
